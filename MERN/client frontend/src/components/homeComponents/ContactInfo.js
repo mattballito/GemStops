77e4e26aa78df9295c5c2070609c162596d0bcc6
mallballito <mbutner@csu.fullerton.edu>
2022-04-30 15:02:28 -0700

    LaunchTime Baby!

---
+++ MERN/client frontend/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us 24x7</h5>
+            <p>(760) 505 6969</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Headquarter</h5>
+            <p>Anaheim Hills, CA</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Fax</h5>
+            <p>555-123-4567</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
